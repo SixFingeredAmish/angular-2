d3f74f701a9fc8028e6fc6ef24da2e84d3895863
nirawit <sixfingeredamish>
2015-11-02 14:00:38 -0800

    blah

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,9 @@
 // Create application
-
+var myApp = angular.module("myApp", [])
 
 // Bind controller madCtrl
-
+myApp.controller("madCtrl", function($scope){
+    })
 
 	// Set initial values for name, age, location, and food
 
